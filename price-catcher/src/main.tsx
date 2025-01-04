1271a5a87ced122a80ede33fe6c4c2175e8f968c
kalobmj <kalobmurphyjones@gmail.com>
2025-01-04 00:49:36 -0500

    test

--- price-catcher/src/main.tsx
+++ price-catcher/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
